1fc2686f51e6fa0e72d51211e44357e63c4fd41e
danielmhi111 <danielmartinhill@gmail.com>
2024-10-09 21:36:52 +0100

    Update

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 from itertools import product
 from pprint import pprint 
 
+# A Game of Life, played on a 10x10 grid as a test case
+
+# rule 1: any live cell with fewer than two live neighbours dies
+
+# rule 2: any live cell with two or three live neighbours lives on to the next turn
+
+# rule 3: any live cell with more than three live neighbours dies
+
+# rule 4: any dead cell with exactly three live neighbours becomes a live cell on the next turn
+
+
 # a blank 10x10 grid
 blank_grid = [
     [0,0,0,0,0,0,0,0,0,0],
@@ -53,23 +64,26 @@ def count_living_neighbours(neighbour_coords: list[tuple[int,int], ], grid: list
 if __name__ == '__main__':
     grid: list[list[int,],] = test_grid
     cell_coords: tuple[int, int] = (4,5)
-    # row, column = cell_coord
-    # cell: int = get_cell(grid=grid, row=row, column=column)
-    # cell_alive = is_alive(cell)
-
-    # print(cell_alive)
+    row, column = cell_coords
+    cell: int = get_cell(grid=grid, row=row, column=column)
 
     neighbour_coords = get_neighbours_coords(cell_coords=cell_coords)
     num_neighbours_living = count_living_neighbours(neighbour_coords=neighbour_coords, grid=grid)
 
-    # rule 1: any live cell with fewer than two live neighbours dies
-
-    # rule 2: any live cell with two or three live neighbours lives on to the next turn
-
-    # rule 3: any live cell with more than three live neighbours dies
-
-    # rule 4: any dead cell with exactly three live neighbours becomes a live cell on the next turn
+    pprint(grid)
+    print(f"cell at {cell_coords} is {"alive" if is_alive(cell) else "dead"} and has {num_neighbours_living} living neighbours")
 
 
+
+    if num_neighbours_living < 2 or num_neighbours_living >= 4:
+        print("cell dies")
+    elif num_neighbours_living in {2,3} and is_alive(cell):
+        print("cell stays alive if alive")
+    elif num_neighbours_living == 3 and not is_alive(cell):
+        print("cell comes to life if dead")
+    elif num_neighbours_living != 3 and not is_alive(cell):
+        print("cell will remain dead")
+
+    print("done")
 
 
